a987b1f6c09c89686dcaf1b34df15c19b175ec28
anilkumarhp <hpanilkumar15@gmail.com>
2024-07-22 11:50:44 +0530

    initial setup done and components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Nav.js"
+import Intro1 from './components/Intro1';
+import Intro2 from './components/intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
+import Promo from './components/Promo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Footer />
     </div>
   );
 }
